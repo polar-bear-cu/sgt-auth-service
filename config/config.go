58dece557fb7add4fefbf6078c0165b0972102f6
BadLuckZ <thanagorn8802@gmail.com>
2026-09-10 22:27:45 +0700

    chore: config

---
+++ config/config.go
@@ -0,0 +1,76 @@
+package config
+
+import (
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/joho/godotenv"
+)
+
+type Config struct {
+	Port            string
+	JWTSecret       string
+	AccessTTL       time.Duration
+	RefreshTTL      time.Duration
+	UserServiceAddr string
+	Google          GoogleConfig
+	DB              DBConfig
+}
+
+type GoogleConfig struct {
+	ClientID     string
+	ClientSecret string
+	RedirectURL  string
+}
+
+type DBConfig struct{ Host, Port, User, Password, Name, SSLMode string }
+
+func (db DBConfig) DSN() string {
+	return fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=%s",
+		db.User, db.Password, db.Host, db.Port, db.Name, db.SSLMode)
+}
+
+func Load() (*Config, error) {
+	_ = godotenv.Load()
+	cfg := &Config{
+		Port:            env("PORT", "8080"),
+		JWTSecret:       os.Getenv("JWT_SECRET"),
+		AccessTTL:       envDuration("ACCESS_TTL", 15*time.Minute),
+		RefreshTTL:      envDuration("REFRESH_TTL", 720*time.Hour),
+		UserServiceAddr: env("USER_SERVICE_ADDR", "localhost:50052"),
+		Google: GoogleConfig{
+			ClientID:     os.Getenv("GOOGLE_CLIENT_ID"),
+			ClientSecret: os.Getenv("GOOGLE_CLIENT_SECRET"),
+			RedirectURL:  env("GOOGLE_REDIRECT_URL", "http://localhost:8084/api/v1/auth/google/callback"),
+		},
+		DB: DBConfig{
+			Host:     env("DB_HOST", "localhost"),
+			Port:     env("DB_PORT", "5435"),
+			User:     env("DB_USER", "postgres"),
+			Password: os.Getenv("DB_PASSWORD"),
+			Name:     env("DB_NAME", "auth"),
+			SSLMode:  env("DB_SSLMODE", "disable"),
+		},
+	}
+	if cfg.JWTSecret == "" {
+		return nil, fmt.Errorf("JWT_SECRET required")
+	}
+	return cfg, nil
+}
+
+func env(k, def string) string {
+	if v := os.Getenv(k); v != "" {
+		return v
+	}
+	return def
+}
+
+func envDuration(k string, def time.Duration) time.Duration {
+	if v := os.Getenv(k); v != "" {
+		if d, err := time.ParseDuration(v); err == nil {
+			return d
+		}
+	}
+	return def
+}
--- main.go
+++ main.go
@@ -2,13 +2,12 @@ package main
 
 import (
 	"log"
-	"os"
-	"time"
 
 	"github.com/gin-gonic/gin"
 	"golang.org/x/oauth2"
 	"golang.org/x/oauth2/google"
 
+	"github.com/polar-bear-cu/sgt-auth-service/config"
 	"github.com/polar-bear-cu/sgt-auth-service/controllers"
 	"github.com/polar-bear-cu/sgt-auth-service/repositories"
 	"github.com/polar-bear-cu/sgt-auth-service/routes"
@@ -16,43 +15,28 @@ import (
 )
 
 func main() {
+	cfg, err := config.Load()
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	oauthCfg := &oauth2.Config{
-		ClientID:     os.Getenv("GOOGLE_CLIENT_ID"),
-		ClientSecret: os.Getenv("GOOGLE_CLIENT_SECRET"),
-		RedirectURL:  os.Getenv("GOOGLE_REDIRECT_URL"),
+		ClientID:     cfg.Google.ClientID,
+		ClientSecret: cfg.Google.ClientSecret,
+		RedirectURL:  cfg.Google.RedirectURL,
 		Scopes:       []string{"openid", "email", "profile"},
 		Endpoint:     google.Endpoint,
 	}
 
-	accessTTL := envDuration("ACCESS_TTL", 15*time.Minute)
-	refreshTTL := envDuration("REFRESH_TTL", 720*time.Hour)
-
 	refresh := repositories.NewInMemoryRefreshToken()
-	uc := usecases.NewAuth(oauthCfg, os.Getenv("JWT_SECRET"), accessTTL, refreshTTL, refresh)
+	uc := usecases.NewAuth(oauthCfg, cfg.JWTSecret, cfg.AccessTTL, cfg.RefreshTTL, refresh)
 	authCtrl := controllers.NewAuth(uc)
 
 	r := gin.Default()
 	routes.Register(r, authCtrl)
 
-	port := envDefault("PORT", "8080")
-	log.Println("listening :" + port)
-	if err := r.Run(":" + port); err != nil {
+	log.Println("listening :" + cfg.Port)
+	if err := r.Run(":" + cfg.Port); err != nil {
 		log.Fatal(err)
 	}
-}
-
-func envDefault(k, def string) string {
-	if v := os.Getenv(k); v != "" {
-		return v
-	}
-	return def
-}
-
-func envDuration(k string, def time.Duration) time.Duration {
-	if v := os.Getenv(k); v != "" {
-		if d, err := time.ParseDuration(v); err == nil {
-			return d
-		}
-	}
-	return def
 }
